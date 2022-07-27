60e02d2abce37f7e8d70dce2ac087a5c428219ca
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-07-28 01:28:15 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [0 for _ in range(n + 1)]
 
-dp = [0 for _ in range(n+1)]
+for i in range(n):
+    for j in range(i + arr[i][0], n+1):
+        if dp[j] < dp[i] + arr[i][1]:
+            dp[j] = dp[i] + arr[i][1]
 
-for i in range(n-1, -1, -1):
-    if i + arr[i][0] > n:
-        dp[i] = dp[i+1]
-    else:
-        dp[i] = max(arr[i][1] + dp[i + arr[i][0]], dp[i+1])
-
-print(dp[0])
+print(dp[-1])
