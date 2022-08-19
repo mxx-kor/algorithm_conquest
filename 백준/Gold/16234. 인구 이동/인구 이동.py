44e355551025d9a3a814d435cf6c4d8e879a87a1
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-08-19 23:48:04 +0900

    [Gold V] Title: 인구 이동, Time: 1088 ms, Memory: 130872 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -4,47 +4,45 @@ from collections import deque
 
 n, l, r = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def bfs(x, y, index):
+def bfs(a, b):
     q = deque()
-    q.append((x, y))
-    summary = graph[x][y]
-    cnt = 1
-    unitied = []
-    unitied.append((x, y))
-    union[x][y] = index
-
+    temp = []
+    q.append((a, b))
+    temp.append((a, b))
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if (0 <= nx < n) and (0 <= ny < n) and (union[nx][ny] == -1):
-                if l <= (abs(graph[nx][ny] - graph[x][y])) <= r:
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0:
+                # 국경선을 공유하는 두 나라의 인구 차이가 L명 이상, R명 이하라면, 두 나라가 공유하는 국경선을 오늘 하루 동안 연다.
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    visited[nx][ny] = 1
                     q.append((nx, ny))
-                    union[nx][ny] = index
-                    summary += graph[nx][ny]
-                    cnt += 1
-                    unitied.append((nx, ny))
-    for i, j in unitied:
-        graph[i][j] = summary // cnt
-    return cnt
+                    temp.append((nx, ny))
+    return temp
+
 
 ans = 0
-
 while True:
-    union = [[-1] * n for _ in range(n)]
-    index = 0
+    visited = [[0] * n for _ in range(n)]
+    flag = 0
     for i in range(n):
         for j in range(n):
-            if union[i][j] == -1:
-                bfs(i, j, index)
-                index += 1
+            if visited[i][j] == 0:
+                visited[i][j] = 1
+                country = bfs(i, j)
+                if len(country) > 1:
+                    flag = 1
+                    num = sum([graph[x][y] for x, y in country]) // len(country)
+                    for x, y in country:
+                        graph[x][y] = num
 
-    if index == n * n:
+    if flag == 0:
         break
     ans += 1
-
 print(ans)
