0fe329af1d405c9ca6fe3e7402df8fc84ce92b26
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-08-23 02:02:48 +0900

    Update 줄 서는 방법.js

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -20,15 +20,3 @@ function solution(n, k) {
     }
     return answer;
 }
-
-
-// import math
-// def solution(n, k):
-//     answer = []
-//     arr = [i for i in range(1, n + 1)]
-//     print(-1//1)
-//     while(arr):
-//         answer.append(arr.pop((k-1) // math.factorial(n-1)))
-//         k = k % math.factorial(n-1)
-//         n -= 1
-//     return answer
