2cc50cfa88f536d85ac6ff57eeec013e3e961461
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-09-04 17:03:54 +0900

    [level 2] Title: 땅따먹기, Time: 40.86 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,13 +1,9 @@
 function solution(land) {
-    for (let i = 0; i < land.length; i++) {
+    for (let i = 1; i < land.length; i++) {
         for (let j = 0; j < land[i].length; j++) {
-            if (i === 0) {
-                continue;
-            } else {
-                let arr = land[i - 1].slice();
-                arr[j] = 0;
-                land[i][j] += Math.max(...arr);
-            }
+            let arr = land[i - 1].slice();
+            arr[j] = 0;
+            land[i][j] += Math.max(...arr);
         }
     }
 
