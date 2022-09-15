34915b4ffef64552035c2e73802aed5f437cec96
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-09-16 00:13:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 13.84 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let [tmp, cnt] = [0, 0]
+    console.log(s.replace(/0/g, ""))
+    while (s !== '1') {
+        let sLen = s.length
+        s = s.replace(/0/g, "").length
+        cnt += sLen - s
+        s = s.toString(2)
+        tmp ++
+    }
+    return [tmp, cnt];
+}
