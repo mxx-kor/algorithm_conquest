759c57d47357883f8aa9dda536687aa17737de93
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2022-09-24 12:25:51 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+V = int(input())
+cute = 0
+
+for _ in range(V):
+    if int(input()) == 1:
+        cute += 1
+
+if cute > V//2:
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
