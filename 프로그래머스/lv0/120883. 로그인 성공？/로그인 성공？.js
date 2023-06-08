f179ee91320bac682063e056db4195d35ac80c1c
MINJAE KIM <minjae7728@gmail.com>
2023-06-08 22:45:24 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,9 +1,6 @@
 function solution(id_pw, db) {
     const [id, pw] = id_pw
-    const users = new Map()
-    db.map((user) => {
-        users.set(user[0], user[1])
-    })
+    const users = new Map(db)
     if (!users.get(id)) return 'fail'
     if (users.get(id) === pw) return 'login'
     return 'wrong pw'
